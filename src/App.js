b7267493cd44eebad3d415a5b388c28458b8fe7f
Boop-Py <boopthecode@outlook.com>
2024-04-06 17:49:32 +0100

    basic project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Main from './components/Main'
+
+import { Container } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Header />
+      <Main />
+    </Container>
+  );
+}
+
+export default App;
